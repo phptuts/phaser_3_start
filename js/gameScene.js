87874854bc10df4db8b809bb62afcf7b7de4b205
phptuts <noah.php@gmail.com>
2018-08-04 16:15:41 -0700

    es6 functions

--- js/gameScene.js
+++ js/gameScene.js
@@ -16,25 +16,23 @@ let getParameterByName = (name) => {
 let gameScene = new Phaser.Scene('Game');
 
 // Runs first and is used to set all the variables
-gameScene.init = function() {
+gameScene.init = () => {	
 };
 
 // Runs second and is used to set all the images used in the game
-gameScene.preload = function() {
+gameScene.preload = () => {
 	// gameScene.load.image('key', 'image_location');
 
 };
 
 // Runs third and is used to position all the images initially in the game
-gameScene.create = function() {
-		// gameScene.add.sprite('key', 'image_location');
-		// setOrigin() changes the center of the image
+gameScene.create = () =>  {
 };
 
 // Runs after every frame and is used to check 
 // conditions and control things in thing in the game
 // This may run 60 times per second and is the last thing that is ran
-gameScene.update = function() {
+gameScene.update = () =>  {
 	
 };
 
